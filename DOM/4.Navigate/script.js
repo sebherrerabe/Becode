113e57016a54a8bed63db4e42b40c5f38750ecdb
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-01-31 16:44:15 +0100

    Navigate done

--- DOM/4.Navigate/script.js
+++ DOM/4.Navigate/script.js
@@ -1,2 +1,14 @@
-/*
-*/
+const docOl = document.querySelector('ol'),
+OlChildren = docOl.children,
+OlLastChild = OlChildren[(OlChildren.length - 1)],
+docSections = document.querySelectorAll('section'),
+docSecondSection = docSections[1],
+docThirdSection = docSections[2],
+docSecSecH2 = docSecondSection.children[0],
+docThSecDIV = docThirdSection.children[0],
+docThSecDIVH2 = docThSecDIV.children[0]
+
+docSecondSection.prepend(docThSecDIVH2);
+docThSecDIV.prepend(docSecSecH2);
+docThirdSection.remove()
+docOl.prepend(OlLastChild)
