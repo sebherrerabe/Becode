06c46487f0d3cb4d70166c5549973438b3c11b60
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-02-01 11:01:47 +0100

    RGB colors fixed

--- DOM/3.Create/script.js
+++ DOM/3.Create/script.js
@@ -26,46 +26,39 @@ const listOfStudents = [
 
 for (const studentName of listOfStudents) {
   const docNewSection = document.createElement("section");
-  docNewSection.style.backgroundColor = `rgb(${Math.random() * 256} ${
-    Math.random() * 256
-  } ${Math.random() * 256})`;
-  const newColorArray = docNewSection.style.backgroundColor;
-  let rgb;
-  if (newColorArray.length === 16) {
-    rgb = newColorArray.substring(4, 15);
-  } else if (newColorArray.length === 17) {
-    rgb = newColorArray.substring(4, 16);
-  } else if (newColorArray.length === 18) {
-    rgb = newColorArray.substring(4, 17);
-  } else if (newColorArray.length === 15) {
-    rgb = newColorArray.substring(4, 14);
-  } else if (newColorArray.length === 14) {
-    rgb = newColorArray.substring(4, 13);
-  } else if (newColorArray.length === 13) {
-    rgb = newColorArray.substring(4, 12);
-  } else if (newColorArray.length === 12) {
-    rgb = newColorArray.substring(4, 11);
-  }
-  let finalRGB = rgb.split(", "),
-    r = finalRGB[0],
+  const getNewNumber = () => {
+    let newNumber = parseInt(Math.random() * 256);
+    return(newNumber);
+  };
+
+  let rgb,
+    finalRGB = [];
+  finalRGB.push(getNewNumber()),
+    finalRGB.push(getNewNumber()),
+    finalRGB.push(getNewNumber());
+  console.log(finalRGB);
+  let r = finalRGB[0],
     g = finalRGB[1],
     b = finalRGB[2],
     hsp = Math.sqrt(0.299 * (r * r) + 0.587 * (g * g) + 0.114 * (b * b));
 
+  docNewSection.style.backgroundColor = `rgb(${finalRGB[0]} ${finalRGB[1]} ${finalRGB[2]})`;
+
   const docNewP = document.createElement("p");
   docNewP.innerText = studentName;
+
+  docNewSection.appendChild(docNewP);
+
   if (hsp > 127.5) {
     docNewP.style.color = "black";
   } else {
     docNewP.style.color = "white";
   }
-  docNewSection.appendChild(docNewP);
-  listOfSections.push(docNewSection)
-  
-console.log(listOfSections)
+  listOfSections.push(docNewSection);
+
 }
 const shuffledArray = listOfSections.sort((a, b) => 0.5 - Math.random());
 
 for (const element of shuffledArray) {
-    docArticle.appendChild(element)
+  docArticle.appendChild(element);
 }
