918bf42705fa2bf3065de3faa9e9f0f8ed9145ae
sebherrerabe <sebastianherrerac1997@gmail.com>
2022-02-08 14:57:55 +0100

    history added to calculator

--- DOM/Projects/3.Calculator/Calculator Project/script.js
+++ DOM/Projects/3.Calculator/Calculator Project/script.js
@@ -7,6 +7,7 @@ const opList = [
   { symbol: "=" },
 ];
 
+const historyList = [{operation: "325 + 956", result: "596"}]
 
 let main = document.querySelector("main"),
   container = document.createElement("div"),
@@ -15,9 +16,17 @@ let main = document.querySelector("main"),
   displayMemoryContainer = document.createElement("div"),
   btContainer = document.createElement("div"),
   nbContainer = document.createElement("div"),
-  opContainer = document.createElement("div");
+  opContainer = document.createElement("div"),
+  historyContainer = document.createElement("div"),
+  topHistory = document.createElement('div'),
+  historyContent = document.createElement('div'),
+  historyul = document.createElement('ul'),
+  titleHistory = document.createElement('h2')
 
-
+  historyul.setAttribute('class', 'history-ul')
+  historyContent.setAttribute('class', 'history-content')
+  topHistory.setAttribute('class', 'top-history')
+  historyContainer.setAttribute("class", "history-container")
   opContainer.setAttribute("class", "op-container");
   nbContainer.setAttribute("class", "nb-container");
   display.setAttribute("class", "display");
@@ -35,6 +44,19 @@ let displayVar = "0",
 
   const nbList = [7,8,9,4,5,6,1,2,3,0];
 
+  const addHistory = (newOperation, newResult) => {
+    let newObject = {operation: newOperation, result: newResult}
+    historyList.push(newObject)
+    let newP = document.createElement('p'),
+    newh3 = document.createElement('h3'),
+    newLi = document.createElement('li');
+    newP.innerText = newObject.operation
+    newh3.innerText = newObject.result
+    newLi.appendChild(newP)
+    newLi.appendChild(newh3)
+    historyul.appendChild(newLi)
+}
+
 
 
 for (const i of nbList) {
@@ -42,12 +64,6 @@ for (const i of nbList) {
   newBt.innerHTML = i;
   newBt.setAttribute('class','button')
   nbContainer.appendChild(newBt);
-  const test = (u,e) => {
-    if (e.key === u) {
-        alert(u)
-    }
-}
-  document.body.addEventListener('keydown',test(i))
   newBt.addEventListener("click", () => {
     if (displayVar === "0") {
       displayVar = `${i}`;
@@ -82,6 +98,7 @@ for (let i = 0; i < opList.length; i++) {
         operation = displayMemory + displayVar;
         displayMemory = displayMemory + " " + displayVar + " =";
         let myResult = Function(`return( ${operation} )`)();
+        addHistory(displayMemory,myResult)
         displayVar = myResult;
         displayMemoryContainer.innerHTML = displayMemory;
         display.innerHTML = displayVar;
@@ -103,6 +120,8 @@ for (let i = 0; i < opList.length; i++) {
         operation = displayMemory + displayVar;
         let myResult = Function(`return( ${operation} )`)();
         displayMemory = myResult + " " + opList[i].symbol;
+        let memoryHistory = displayMemory + " " + displayVar + " ="
+        addHistory(memoryHistory,myResult)
         displayMemoryContainer.innerHTML = displayMemory;
         displayVar = "0";
       } else {
@@ -117,9 +136,14 @@ for (let i = 0; i < opList.length; i++) {
   }
 
 
+titleHistory.innerText = "History"
 
 display.innerHTML = displayVar;
 
+historyContent.appendChild(historyul)
+topHistory.appendChild(titleHistory)
+historyContainer.appendChild(topHistory)
+historyContainer.appendChild(historyContent)
 btContainer.appendChild(nbContainer);
 btContainer.appendChild(opContainer);
 displayContainer.appendChild(displayMemoryContainer)
@@ -127,3 +151,4 @@ displayContainer.appendChild(display)
 container.appendChild(displayContainer);
 container.appendChild(btContainer);
 main.appendChild(container);
+main.appendChild(historyContainer)
